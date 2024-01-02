a15bc0c2216ca49cb947dabc9eefa90606020b25
suleduran <sulekvsrr@gmail.com>
2024-01-02 22:50:49 +0300

    kategori ve ürün componentleri eklendi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
 import Header from './components/Layout/Header/Header';
 import Slider from './components/Slider/Sliders';
+import Categories from './components/Categories/Categories';
+import Products from './components/Products/Products';
 import Policy from './components/Layout/Policy/Policy';
 import Footer from './components/Layout/Footer/Footer';
-
+import './App.css'
 function App() {
 
   return (
     <div>
       <Header/>
      <Slider/>
+     <Categories/>
+     <Products/>
      <Policy/>
      <Footer/>
     </div>
