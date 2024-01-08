5ed82a7681f2dcb354a018a2fbbef038ec51a5e3
suleduran <sulekvsrr@gmail.com>
2024-01-08 23:30:57 +0300

    Search modal yapıldı

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-//  import HomePage from './pages/HomePage';
+ import HomePage from './pages/HomePage';
 // import ShopPage from './pages/ShopPage';
 // import ContactPage from './pages/ContactPage';
 // import AuthPage from './pages/AuthPage';
 // import CartPage from './pages/CartPage';
 import './App.css'
 // import ProductsDetailsPage from './pages/ProductsDetailsPage';
-import BlogDetailsPage from './pages/BlogDetailsPage';
+// import BlogDetailsPage from './pages/BlogDetailsPage';
 // import BlogPage from './pages/BlogPage';
 
 function App() {
 
   return (
     <React.Fragment>
-      {/* <HomePage /> */}
+      <HomePage />
       {/* <ShopPage /> */}
       {/* <ContactPage /> */}
       {/* <AuthPage/> */}
       {/* <CartPage/> */}
       {/* <BlogPage/> */}
-      <BlogDetailsPage/>
+      {/* <BlogDetailsPage/> */}
       {/* <ProductsDetailsPage/> */}
     </React.Fragment>
   );
